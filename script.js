8ef66d4481fec0ab4d930a794d6e9d15df51decf
savio-operator <saviojossy0509@gmail.com>
2026-03-04 02:56:42 +0530

    run.11

---
+++ script.js
@@ -0,0 +1,31 @@
+// Carousel Functionality
+const track = document.querySelector('.carousel-track');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-track img');
+const totalImages = images.length;
+
+nextBtn.addEventListener('click', ()=> {
+    if (currentIndex < totalImages -1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateCarosuel();
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = totalImages - 1;
+    }
+    updateCarosuel();
+});
+
+function updateCarosuel() {
+    const imageWidth = images[0].offsetWidth + 20;
+    track.style.transform = 'translateX(-${currentIndex * imageWidth}px)';
+}
