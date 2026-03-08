4f347803de47c583c6757ebceba026ad5985ed0b
SAVIO <264572907+savio-operator@users.noreply.github.com>
2026-03-08 21:52:43 +0530

    run.33

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// Carousel functionality
-const track = document.querySelector('.carousel-track');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-
-let currentIndex = 0;
-const images = document.querySelectorAll('.carousel-track img');
-const totalImages = images.length;
-
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < totalImages - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    updateCarousel();
-});
-
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = totalImages - 1;
-    }
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const imageWidth = 420;
-    track.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
-}
-
-setInterval(() => { nextBtn.click(); }, 3000);
 
 // Menu tab filtering
 const tabBtns = document.querySelectorAll('.tab-btn');
